2a83f447911dbf58dc77b95ba92c5fb92db7c082
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-09 07:24:30 +0600

    Update lineage_lava.mk

--- lineage_lava.mk
+++ lineage_lava.mk
@@ -27,6 +27,8 @@ $(call inherit-product, packages/apps/Viper4FX/config.mk)
 # Inherit AnxCam 
 $(call inherit-product, vendor/ANXCamera/config.mk)
 
+#Gapps
+WITH_GMS=true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
