e7d56666873ef0bc98901c008993997019e171d6
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-17 12:29:07 +0600

    Update and rename lineage_lava.mk to aospa_lava.mk

--- aospa_lava.mk
+++ aospa_lava.mk
@@ -42,7 +42,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 #
 # All components inherited here go to system_ext image
@@ -52,7 +52,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lava
-PRODUCT_NAME := lineage_lava
+PRODUCT_NAME := aospa_lava
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := xiaomi
 
