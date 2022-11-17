baef5437729312644b698fcffc59c0b32b980e27
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-17 12:26:51 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/aospa_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    aospa_lava-user \
+    aospa_lava-userdebug \
+    aospa_lava-eng
